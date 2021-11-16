656b968dc19c916b6518b6e821239620b9beb731
Sergey Dvoraninovich <sergey.dvoraninovich@gmail.ru>
2021-11-17 02:50:53 +0300

    orders schema fix

---
+++ repository/src/main/java/com/epam/esm/repository/Impl/OrderItemRepositoryImpl.java
@@ -0,0 +1,2 @@
+package com.epam.esm.repository.Impl;public class OrderItemPepositoryImpl {
+}
---
+++ service/src/main/java/com/epam/esm/dto/mapping/OrderItemDtoMapper.java
@@ -0,0 +1,28 @@
+package com.epam.esm.dto.mapping;
+
+import com.epam.esm.dto.GiftCertificateDto;
+import com.epam.esm.dto.OrderItemDto;
+import com.epam.esm.entity.OrderItem;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+
+@Component
+@RequiredArgsConstructor
+public class OrderItemMapper {
+    private final ModelMapper mapper;
+    private final GiftCertificateDtoMapper giftCertificateDtoMapper;
+
+    public OrderItem toEntity(OrderItemDto dto) {
+        return Objects.isNull(dto) ? null : mapper.map(dto, OrderItem.class);
+    }
+
+    public OrderItemDto toDto(OrderItem entity) {
+        OrderItemDto dto = Objects.isNull(entity) ? null : mapper.map(entity, OrderItemDto.class);
+        GiftCertificateDto certificateDto = giftCertificateDtoMapper.toDto(entity.getGiftCertificate());
+        dto.setGiftCertificate(certificateDto);
+        return dto;
+    }
+}
