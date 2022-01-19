bbc648db0ab163ade50b42e851f41d5d1da55769
Sergey-Dvoraninovich <sergey.dvoraninovich@yandex.ru>
2022-01-19 18:11:30 +0300

    minor update

--- service/src/test/java/com/epam/esm/service/GiftCertificateServiceTest.java
+++ service/src/test/java/com/epam/esm/service/GiftCertificateServiceTest.java
@@ -87,7 +87,7 @@ public class GiftCertificateServiceTest {
 //        assertEquals(expected, actual);
 
         assertFalse(false);
-        assertFalse(false);
+//        assertFalse(false);
     }
 
     @Test
